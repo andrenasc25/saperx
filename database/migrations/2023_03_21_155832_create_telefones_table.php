693d323e0a9dab66c5197ca1474e0d9e8cc84cf0
andre-rep <guerreiroandre25@gmail.com>
2023-03-21 13:00:24 -0300

    migrations para telefones criado

---
+++ database/migrations/2023_03_21_155832_create_telefones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTelefonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefones', function (Blueprint $table) {
+            $table->id();
+            $table->string('telefone');
+            $table->unsignedBigInteger('contato_id');
+            $table->timestamps();
+
+            $table->foreign('contato_id')->references('id')->on('contatos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefones');
+    }
+}
